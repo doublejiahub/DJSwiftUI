0fca2e3367c463716b7ba6f0c6c32cb899f1d77e
haojiajia <haojiajia@zhihu.com>
2021-06-21 20:00:03 +0800

    添加播放器

---
+++ DJSwiftUI/DJSwiftUI/LoginView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginView.swift
+//  DJSwiftUI
+//
+//  Created by zhihu on 2021/6/21.
+//
+
+import Foundation
---
+++ DJSwiftUI/DJSwiftUI/VideoPlayer.swift
@@ -0,0 +1,31 @@
+//
+//  VideoPlayer.swift
+//  DJSwiftUI
+//
+//  Created by zhihu on 2021/6/21.
+//
+
+import SwiftUI
+import AVKit
+
+struct SUIVideoPlayer: View {
+    private let player = AVPlayer(url: URL(string: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8")!)
+    
+    var body: some View {
+        VideoPlayer(player: player)
+            .onAppear() {
+                // Start the player going, otherwise controls don't appear
+                player.play()
+            }
+            .onDisappear() {
+                // Stop the player when the view disappears
+                player.pause()
+            }
+    }
+}
+
+struct SUIVideoPlayer_Previews: PreviewProvider {
+    static var previews: some View {
+        SUIVideoPlayer()
+    }
+}
